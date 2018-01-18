7cee30d46176bce3a59d2c1affb8d5cebe74dac6
oriTid <ori@adidance.co.il>
2018-01-18 16:06:04 +0200

    g

--- HomeWork_Lesson_17/app.js
+++ HomeWork_Lesson_17/app.js
@@ -2,11 +2,15 @@
 //let pizza2: Pizza = new Pizza();
 //pizza1.printPizza();
 //pizza2.printPizza();
-var PizzaArr = new Array(5);
+var numOfPiz = parseInt(prompt("Hello Im the Pizza Robot.\nHow many Pizza's would you like me to bake for you today ?"));
+while (numOfPiz <= 0) {
+    numOfPiz = parseInt(prompt("Are you serious !!??\n You cant ask me to bake " + numOfPiz + " Pizzas.\nPlease try again."));
+}
+var PizzaArr = new Array(numOfPiz);
 var PizzaNumber = 0;
 for (var i = 0; i < PizzaArr.length; i++) {
     PizzaArr[i] = new Pizza();
-    document.write("--------------------- Pizza number " + i + " : ---------------------------<br/>");
+    document.write("--------------------- Pizza number " + (i + 1) + " : ---------------------------<br/>");
     PizzaArr[i].printPizza();
     ++PizzaNumber;
 }
--- HomeWork_Lesson_17/app.ts
+++ HomeWork_Lesson_17/app.ts
@@ -3,17 +3,23 @@
 //pizza1.printPizza();
 //pizza2.printPizza();
 
-let PizzaArr: Array<Pizza> = new Array<Pizza>(5);
-let PizzaNumber: number = 0;
-
-for (let i: number = 0; i < PizzaArr.length; i++) {
-    PizzaArr[i] = new Pizza();
-    document.write(`--------------------- Pizza number ${i} : ---------------------------<br/>`)
-    PizzaArr[i].printPizza();
-    ++PizzaNumber;
-
+let numOfPiz: number = parseInt(prompt(`Hello Im the Pizza Robot.\nHow many Pizza's would you like me to bake for you today ?`));
+while (numOfPiz <= 0) {
+    numOfPiz = parseInt(prompt(`Are you serious !!??\nYou cant ask me to bake ${numOfPiz} Pizzas.\nPlease try again.`));
 }
 
-document.write(`Sum of all ${PizzaNumber} Pizza's topping is : ${Pizza.sumOfTopics()}`);
+    let PizzaArr: Array<Pizza> = new Array<Pizza>(numOfPiz);
+    let PizzaNumber: number = 0;
+
+    for (let i: number = 0; i < PizzaArr.length; i++) {
+        PizzaArr[i] = new Pizza();
+        document.write(`--------------------- Pizza number ${i+1} : ---------------------------<br/>`)
+        PizzaArr[i].printPizza();
+        ++PizzaNumber;
+
+    }
+    document.write(`Sum of all ${PizzaNumber} Pizza's topping is : ${Pizza.sumOfTopics()}`);
+
+
 
 
